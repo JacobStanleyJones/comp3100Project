2a68a6bdec365fdf0acf4570cd965e90c26750dd
Jacob Stanley-Jones <cobbysj@gmail.com>
2022-05-26 02:33:48 +0000

    First attempt of speedy algorithm

--- MyClient.java
+++ MyClient.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.net.*;
 
+import javax.lang.model.util.ElementScanner6;
+
 public class MyClient {
 
 	static Socket s;
@@ -19,8 +21,24 @@ public class MyClient {
 
 			Handshake();
 
-			// ScheduleLRR();
-			ScheduleFC();
+			Boolean useDefaultAlgorithm = true;
+			for(int i = 0; i < args.length; i++){
+				if(args[i].equals("fc")){
+					useDefaultAlgorithm = false;
+					ScheduleFC();
+					break;
+				}else if(args[i].equals("lrr")){
+					useDefaultAlgorithm = false;
+					ScheduleLRR();
+					break;
+				}else if(args[i].equals("spd")){
+					useDefaultAlgorithm = false;
+					ScheduleSPD();
+					break;
+				}
+			}
+			if(useDefaultAlgorithm)
+				ScheduleLRR();
 
 			dout.write(("QUIT\n").getBytes());
 
@@ -85,6 +103,64 @@ public class MyClient {
 		}catch(Exception e){
 			System.out.println(e);
 		}
+	}
+
+	//Speedy
+	//Schedules all jobs to the largest (most cores) server in a round robin fashion.
+	private static void ScheduleSPD(){
+		try{
+			String str = "";
+			int jobNum = 0;
+				while(!str.equals("NONE")){
+					dout.write(("REDY\n").getBytes());
+					str = (String) in.readLine();
+					System.out.println("message= " + str);
+					
+					if(servers == null)
+						GetServerData();
+	
+					String[] jobData = str.split(" ");
+					
+					//If it's a job submission, schedule it to the largest server
+					if(jobData[0].equals("JOBN") || jobData[0].equals("JOBP")){
+
+						//check if all the largest ones are inUse. If they are 
+						String idealServer = null;
+						int idealCores = 0;
+						for(int i = 0; i < servers.length; i++){
+							Server server = servers[i];
+							if(server.sCores > Integer.parseInt(jobData[4]) && !server.inUse){
+								if(server.sCores > idealCores){
+									idealServer = server.sName();
+									idealCores = server.sCores;
+								}
+							}
+						}
+
+						if(idealServer == null){
+							idealServer = servers[largestServerIdx].sType + " " + jobNum % largestServerLimit;
+							jobNum++;
+						}
+							
+						String sch = "SCHD " + jobData[2] + " " + idealServer + "\n";
+						dout.write((sch).getBytes());
+
+						int idx = getServerIdx(idealServer);
+						servers[idx].inUse = true;
+	
+						str = (String) in.readLine();
+						System.out.println("message= " + str);
+						
+						
+					}else if(jobData[0].equals("JCPL")){
+						int idx = getServerIdx(jobData[3] + " " + jobData[4]);
+						servers[idx].inUse = false;
+					}
+				}
+		}catch(Exception e){
+			System.out.println(e);
+		}
+		
 	}
 
 	//Largest Round Robin
@@ -172,16 +248,33 @@ public class MyClient {
 		}
 		
 	}
+
+	private static int getServerIdx(String name){
+		if(servers == null)
+			return -1;
+
+		for(int i = 0; i < servers.length; i++){
+			if(servers[i].sName().equals(name))
+			return i;
+		}
+		return -1;
+	}
 }
 
 class Server{
 	String sType;
 	int sId;
 	int sCores;
+	Boolean inUse;
 
 	public Server(String type, int id, int cores){
 		sType = type;
 		sId = id;
 		sCores = cores;
+		inUse = false;
+	}
+
+	public String sName(){
+		return sType + " " + sId;
 	}
 }
